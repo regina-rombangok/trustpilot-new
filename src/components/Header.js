998d92fbbe15918a548b2465fc975672c8dd789c
Regina <regina.rombangok@gmail.com>
2022-05-21 09:50:27 +0800

    [WIP]: Header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../styles/Header.css';
+
+function Header() {
+  return (
+    <div className="Header-container">
+      <img src="../assets/logo-white.svg" alt="logo-white" />
+    </div>
+  );
+}
+
+export default Header;
